7c02b2a15348159a1d1e98503a741a855c1e5a93
07deeksha <126002952+07deeksha@users.noreply.github.com>
2025-09-07 12:37:06 +0530

    Delete testingdecoder.py

--- testingdecoder.py
+++
@@ -1,9 +0,0 @@
-from build_vocab import Vocabulary
-import pickle
-with open(r'C:\Users\Deeksha\Desktop\TrendPulse\Tp\vocab.pkl', 'rb') as f:
-    vocab = pickle.load(f)
-    print("done")
-
-with open(r'C:\Users\Deeksha\Desktop\TrendPulse\Tp\decoder-140-1.ckpt', 'rb') as f:
-    vocab = pickle.load(f)
-    print("ok")
